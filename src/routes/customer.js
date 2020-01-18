38af7d57e3ca6487eb63f9cd673c933c4767f8e6
Archit Shinde <archits581@gmail.com>
2020-01-18 11:24:47 +0530

    Backend integration

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -1,33 +1,50 @@
 const express = require('express')
+const ejs = require('ejs')
 const Customer = require('../models/customer')
 const passport = require('passport')
 const {ensureAuthenticated, forwardAuthenticated} = require('../config/auth')
 const router = new express.Router()
 
-router.post('/customer/register', async (req, res)=>{
-    const customer = new Customer(req.body)
+router.post('/customer-signup', async (req, res)=>{
+    const data = req
+    console.log(data)
+    const customer = new Customer({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        age: req.body.age,
+        address: req.body.address,
+        state: req.body.state,
+        phone: req.body.phone
+    }) 
     try{
         await customer.save()
+        console.log('Done')
         res.send(customer)
     }catch(e){
         res.status(400).send('Error in registration')
     }
 })
+router.get('/customer-signup',(req,res)=>{
+    res.render("signup-user");
+});
 
-router.get('/customer/login', (req, res)=>{
-    res.send("login page")
+router.get('/customer-login', (req, res)=>{
+    res.render("login-customer")
 })
 
-router.post('/customer/login', async(req, res, next)=>{
+router.post('/customer-login', async(req, res, next)=>{
+    const data = req.body
+    console.log(data)
     passport.authenticate('local', {
         successRedirect: '/customer/home',
-        failureRedirect: '/customer/login',
+        failureRedirect: '/customer-login',
         failureFlash: false
     })(req, res, next)
 })
 
-router.get('/customer/home', (req, res)=>{
-    res.send('Home Page ')
+router.get('/customer/home', ensureAuthenticated, (req, res)=>{
+    res.render('catalogue  ')
 })
 
 router.get('/customer/:id', async(req, res)=>{
